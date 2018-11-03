5d98f8656f995836bd5c6598b254932fc7d578f5
Dawson Ley <anarchonist@protonmail.com>
2018-11-03 00:26:48 +0000

    I cannot get that old glitch to display no matter what I do...everything is functioning correctly. Code cleaned

--- express_server.js
+++ express_server.js
@@ -1,22 +1,19 @@
 var express = require('express');
-//var cookieParser = require('cookie-parser');
 var cookieSession = require('cookie-session');
 var app = express();
 
 app.use(cookieSession({
   name: 'session',
   keys: ['Keyoooo!']
-
 }));
+
 var PORT = 8080;
-var cookie;
 var currentUser;
 var loggedUser = false;
 var userKey;
 const bcrypt = require('bcrypt');
 
 app.set('view engine', 'ejs');
-// app.use(cookieParser());
 
 function generateRandomString() {
   var randomString = '';
@@ -24,7 +21,6 @@ function generateRandomString() {
   for (var i = 0; i < 6; i++) {
     randomString += Math.ceil(Math.random() * 6);
   }
-
   return randomString;
 }
 //databases
@@ -53,46 +49,35 @@ const users = {
 };
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
-
-//helper functions
-
+//Creates a new object of urls that belong to the logged in user
 function urlsForUser(id) {
-  console.log("id we get", id);
-  console.log("url database ", urlDatabase);
   var finito = {};
   for (var key in urlDatabase) {
-    console.log('comparer :', urlDatabase[key].userID);
-    console.log('ID', id);
+
     if (id === urlDatabase[key].userID) {
-      console.log('im activated!');
       finito[key] = urlDatabase[key].link;
-      console.log('the current user is : ' + currentUser);
     }
   }
-
-  console.log('my object :', finito);
   return finito;
 }
 
 app.get("/urls", (req, res) => {
 
   var filtered = urlsForUser(req.session.user_id);
-  console.log('these are my filtered urls: ', filtered);
-  console.log('this is my url database: ', urlDatabase);
+
   let templateVars = { username: users[req.session["user_id"]],
     urls: filtered
   };
+
   if (loggedUser) {
     res.render("urls_index", templateVars);
   } else {
-    // res.redirect(403, "Looks like somebody isn't logged in....");
     res.status(403).send("Looks like somebody isn't logged in...");
   }
 });
 
 app.get("/urls/new", (req, res) => {
-  console.log('logged user: ', loggedUser);
+
   let templateVars = { username: users[req.session["user_id"]],
     urls: urlDatabase
   };
@@ -100,14 +85,12 @@ app.get("/urls/new", (req, res) => {
   if (loggedUser) {
     res.render("urls_new", templateVars);
   } else {
-    res.render('login');
-
+    res.redirect('/login');
   }
 });
 
 app.get("/urls/:id", (req, res) => {
-  console.log('comparer',urlDatabase);
-  console.log('id:',req.params.id)
+
   let templateVars = { username: users[req.session["user_id"]], shortURL: req.params.id,
     lurl: urlDatabase[req.params.id].link};
   let exists = false;
@@ -133,7 +116,6 @@ app.get('/', (req, res) => {
   } else {
     res.redirect('/login');
   }
-  res.send("<h1>Hello</h1><br><a href='/urls/new'>Make a new url</a>");
 });
 
 app.get('/urls.json', (req, res) => {
@@ -147,7 +129,11 @@ app.get('/hello', (req, res) => {
 app.get('/register', (req, res) => {
   let templateVars = { username: users[req.session.user_id]};
   console.log(templateVars);
-  res.render('registration_page', templateVars);
+  if (loggedUser) {
+    res.status(403).send('You already logged in...no more registration for you!');
+  } else {
+    res.render('registration_page', templateVars);
+  }
 });
 
 app.get('/login', (req, res) => {
@@ -198,9 +184,7 @@ app.post('/login', (req, res) => {
       let pFind = users[usr];
       truMail = users[usr].email;
       if (bcrypt.compareSync(pWord, pFind.password)) {
-      // if (pWord === users[usr].password) {
         req.session.user_id = userKey;
-        // res.cookie('user_id', users[usr].id);
         currentUser = req.session.user_id;
         console.log('Successful login');
         truPass = users[usr].password;
@@ -221,7 +205,6 @@ app.post('/login', (req, res) => {
 
 //logout feature
 app.post('/logout', (req, res) => {
-  console.log('password im comparing to: ', users);
   loggedUser = false;
   req.session.user_id = null;
   res.redirect('/');
@@ -231,7 +214,6 @@ app.post('/logout', (req, res) => {
 app.post('/urls/:id/delete', (req, res) => {
 
   if (req.session.user_id === urlDatabase[req.params.id].userID) {
-    console.log("condition matched for the delete");
     delete urlDatabase[req.params.id];
     res.redirect('/urls');
   } else {
@@ -242,26 +224,28 @@ app.post('/urls/:id/delete', (req, res) => {
 app.post('/urls/:id/update', (req, res) => {
   console.log('update body: ', req.body);
   urlDatabase[req.params.id].link = req.body.urlName;
-  //res.render('/urls/' + req.params.id + '/update');
   res.redirect('/urls/' + req.params.id);
 });
 
 app.post('/urls/:id', (req, res) => {
-  console.log('req.params.id :', req.params.id);
+
   let templateVars = { username: users[req.session["user_id"]]};
-  if (req.session.user_id === urlDatabase[req.params.id].userID) {
+
+  if (loggedUser && req.session.user_id === urlDatabase[req.params.id].userID) {
     urlDatabase[req.params.id] = {link: req.body.urlName};
     res.redirect('/urls');
+  } else if (loggedUser && req.session.user_id !== urlDatabase[req.params.id].userID) {
+    res.status(403).send("Stop tampering with other peoples stuff!");
   } else {
     res.status(403).send('ACCESS DENIED!');
   }
 });
 
 app.post("/urls", (req, res) => {
+
   var stringo = generateRandomString();
-  console.log('This is the long url that didnt show :', req.body.longURL);
   urlDatabase[stringo] = {'link': req.body.longURL, userID: req.session.user_id};///!!!!
-  console.log('whatever goes in userID: ' + req.session.user_id);
+
   if (!loggedUser) {
     res.status(403).send('Log yourself in or get lost buddai!');
   } else {
@@ -270,15 +254,11 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  let proper = false;
-  console.log('Redirect stuff: ', req.params.shortURL);
 
-  for (let url in urlDatabase) {
-    console.log('compare1: ', urlDatabase[url].link);
-    console.log('compare2: ', urlDatabase[req.params.shortURL].link);
-    if (urlDatabase[url].link == urlDatabase[req.params.shortURL].link) {
-      proper = true;
-    }
+  let proper = false;
+
+  if (urlDatabase[req.params.shortURL]) {
+    proper = true;
   }
   if (proper) {
     let longURL = urlDatabase[req.params.shortURL].link;
@@ -286,6 +266,10 @@ app.get("/u/:shortURL", (req, res) => {
   } else {
     res.status(404).send('This url does not exist...leave this place');
   }
+});
+
+app.use((req, res, next) => {
+  res.send("Page not found");
 });
 
 app.listen(PORT, () => {
